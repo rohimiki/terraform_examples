86214cd4c9205142f787ef0c33405298545567f6
shinfuku kimihiro <shinfuku@shinfukunanaminoMac-mini.local>
2021-10-19 20:10:10 +0900

    edit2 dev/main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -23,3 +23,18 @@ resource "aws_subnet" "main" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.0.0/16"
 }
+
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    organization = "example_corp"
+    workspaces = {
+      name = "PROJECT-DEV"
+    }
+  }
+}
+
+resource "aws_instance" "redis_server" {
+  # Terraform 0.12 and later: use the "outputs.<OUTPUT NAME>" attribute
+  subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
+}
